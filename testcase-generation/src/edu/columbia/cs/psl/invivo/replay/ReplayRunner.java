420c9bb893d7e633720b906fad0cca9b27caafbc
Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
2012-07-29 22:07:49 -0400

    Some strange errors persist (fieldnotfound, which is surprising because the field IS there...)

--- testcase-generation/src/edu/columbia/cs/psl/invivo/replay/ReplayRunner.java
+++ testcase-generation/src/edu/columbia/cs/psl/invivo/replay/ReplayRunner.java
@@ -18,18 +18,18 @@ import org.objectweb.asm.ClassWriter;
 public class ReplayRunner extends ClassLoader {
 	public static void run(byte[] code, String className) {		
 		ReplayRunner runner = new ReplayRunner();
-		/*Path path = Paths.get("/home/nikhil/in-vivo/testcase-generation/replayed/edu/columbia/cs/psl/invivo/example/ReaderUserInvivoLog.class");
-System.out.println(className);
+		Path path = Paths.get("/home/nikhil/in-vivo/testcase-generation/instrumented/edu/columbia/cs/psl/invivo/example/ReaderUserInvivoLog.class");
 		try {
-			Class<?> instrumentedClass = runner.defineClass(className+"InvivoLog",Files.readAllBytes(path), 0, Files.readAllBytes(path).length);
+			Class<?> instrumentedClass = runner.defineClass("edu.columbia.cs.psl.invivo.example.ReaderUserInvivoLog",Files.readAllBytes(path), 0, Files.readAllBytes(path).length);
 		} catch (ClassFormatError e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}*/
-		Class<?> replayClass = runner.defineClass(className, code, 0, code.length);
+		}
+		
+		Class<?> replayClass = runner.defineClass("edu.columbia.cs.psl.invivo.example.ReaderUser", code, 0, code.length);
 		
 		try {
 			replayClass.getMethods()[0].invoke(null, new Object[] { null });
