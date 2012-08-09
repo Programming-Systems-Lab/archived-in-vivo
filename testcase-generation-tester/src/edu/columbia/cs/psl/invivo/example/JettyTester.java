b67956f78700ddc0a299576b852755e4e2813651
Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
2012-08-09 16:47:06 -0400

    Jon's changes on my machine

--- testcase-generation-tester/src/edu/columbia/cs/psl/invivo/example/JettyTester.java
+++ testcase-generation-tester/src/edu/columbia/cs/psl/invivo/example/JettyTester.java
@@ -2,12 +2,21 @@ package edu.columbia.cs.psl.invivo.example;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.util.EventListener;
 import java.util.Scanner;
 
 import org.eclipse.jetty.http.HttpParser;
 import org.eclipse.jetty.io.Buffer;
 import org.eclipse.jetty.io.ByteArrayBuffer;
 import org.eclipse.jetty.io.View;
+import org.eclipse.jetty.server.LocalConnector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.server.handler.ErrorHandler;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 
 public class JettyTester {
 
@@ -18,16 +27,16 @@ public class JettyTester {
 			// TODO Auto-generated catch block
 			return str.getBytes();
 		}
-	}
+	}	
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Enter your input: ");
-		String rawText = "HTTP/1.1 200 OK\r\n"+
+		String rawText = scanner.nextLine();/*"HTTP/1.1 200 OK\r\n"+
 	            "Content-Type: text/html\r\n"+
 	            "Content-Length: 22\r\n"+
-	            "\r\n";
+	            "\r\n";*/
 
 		ByteArrayBuffer buf = new ByteArrayBuffer(getByteArray(rawText));
 		View view = new View(buf);
